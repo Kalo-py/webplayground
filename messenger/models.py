0c5371ec9ed1cdc5256f6a45a82554902516d175
Carlos@DESKTOP-CTD8JUA <carlosservian09@gmail.com>
2024-04-14 15:53:09 -0400

    14/04/2024

--- messenger/models.py
+++ messenger/models.py
@@ -29,8 +29,12 @@ class ThreadManager(models.Manager):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+    updated = models.DateTimeField(auto_now=True)
 
     objects = ThreadManager()
+
+    class Meta:
+        ordering=['-updated']
 
 def messages_changed(sender, **kwargs):
     instance = kwargs.pop("instance", None)
@@ -50,6 +54,9 @@ def messages_changed(sender, **kwargs):
 
     #Buscar los mensajes de false_pk_set que si estan en pk_set y los borramos de pk_set
     pk_set.difference_update(false_pk_set)
+
+    #forzar la actualizacion
+    instance.save()
 
 m2m_changed.connect(messages_changed, sender=Thread.messages.through)
 
